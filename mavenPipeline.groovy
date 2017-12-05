991a10f2169d9cf41abbc1ccf24230e7c9dc27b6
lvthillo <lorenz.vanthillo@outlook.com>
2017-12-05 21:32:28 +0100

    initial commit

---
+++ mavenPipeline.groovy
@@ -0,0 +1,21 @@
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+        stage('checkout git') {
+         	steps {
+          		git branch: pipelineParams.branch, credentialsId: 'pipelineParams.scmCredentials', url: pipelineParams.scmUrl
+           	}
+       	}
+
+		stage('build') {
+      		steps {
+				sh 'mvn clean package -DskipTests=true'
+            }
+        }
+    }
+}
