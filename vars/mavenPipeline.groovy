01606547c5ab8c9eed26e8dcc368b011a7b7921f
lvthillo <lorenz.vanthillo@outlook.com>
2017-12-07 22:25:42 +0100

    update pom

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -8,8 +8,12 @@ def call(body) {
     pipeline {
         agent any
 
+        options {
+            buildDiscarder(logRotator(numToKeepStr: '3'))
+        }
+
         environment {
-            VAR = 'test'        
+            POM = 'pom.xml'        
         }
 
         parameters { booleanParam(name: 'SKIP_TESTS', defaultValue: false, description: 'Check if you want to skip tests') }
@@ -20,16 +24,10 @@ def call(body) {
                     git branch: pipelineParams.branch, credentialsId: pipelineParams.scmCredentials, url: pipelineParams.scmUrl
                 }
             }
- 
-            stage('test') {
-                steps {
-                    echo env.VAR
-                } 
-            }
 
             stage('build') {
                 steps {	
-                    sh 'mvn clean package -DskipTests=${EXECUTE_TESTS}'
+                    sh 'mvn clean package -f ${POM} -DskipTests=${SKIP_TESTS}'
                 }
             }
 
@@ -40,7 +38,7 @@ def call(body) {
                         def rtMaven = Artifactory.newMavenBuild()
                         rtMaven.deployer server: server, releaseRepo: 'company-release', snapshotRepo: 'company-snapshot'
                         rtMaven.tool = 'Maven 3.5.2'
-                        def buildInfo = rtMaven.run pom: '${POMPATH}', goals: 'clean install'
+                        def buildInfo = rtMaven.run pom: '${POM}', goals: 'clean install'
                         server.publishBuildInfo buildInfo
                     }
                 }
