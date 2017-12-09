b499b4ab8ee17f5c99627a039f195c3896610b6b
lvthillo <lorenz.vanthillo@outlook.com>
2017-12-09 15:04:29 +0100

    add slacknotfier

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,10 +1,5 @@
 #!/usr/bin/env groovy
 
-def call() {
-    def server = Artifactory.server "jfrog-artifactory"
-    def rtMaven = Artifactory.newMavenBuild()
-    rtMaven.deployer server: server, releaseRepo: 'company-release', snapshotRepo: 'company-snapshot'
-    rtMaven.tool = 'Maven 3.5.2'
-    def buildInfo = rtMaven.run pom: env.POM, goals: 'clean install'
-    server.publishBuildInfo buildInfo
+def call(String buildResult) {
+	println buildResult
 }
