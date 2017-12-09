3a16faff5ad51ed4b1d5a63352a5198dc8427f3f
lvthillo <lorenz.vanthillo@outlook.com>
2017-12-09 15:15:14 +0100

    add slacknotfier plugin integration

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -2,4 +2,7 @@
 
 def call(String buildResult) {
 	println buildResult
+	if ( buildResult == "SUCCES" ) {
+	    slackSend channel: '#jenkins-latest', color: 'good', message: 'Job: ${env.JOB_NAME} BuildNumber ${env.BUILD_NUMBER} was succesful'
+	}
 }
