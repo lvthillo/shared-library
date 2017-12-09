50d4a30f5c12a9d28809903e35d100577d3e5a5a
lvthillo <lorenz.vanthillo@outlook.com>
2017-12-09 15:34:04 +0100

    edit message

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,10 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String buildResult) {
-	println env.JOB_NAME	
-	println currentBuild.currentResult
-	println "hey"
 	if ( buildResult == "SUCCESS" ) {
-	    slackSend color: 'good', message: 'Job: env.JOB_NAME BuildNumber env.BUILD_NUMBER was successful'
+	    slackSend color: "good", message: "Job: ${env.JOB_NAME} BuildNumber ${env.BUILD_NUMBER} was successful"
 	}
 }
